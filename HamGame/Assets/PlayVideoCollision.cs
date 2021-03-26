4230979275ca6984c326c37b2c94aba28cadac04
Alexandra Melançon <43081750+alexMelancon@users.noreply.github.com>
2021-03-26 15:41:44 -0400

    Added video player mechanic
    Added everything in the Video scene with the code and video file in the Assets folder.

---
+++ HamGame/Assets/PlayVideoCollision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideoCollision : MonoBehaviour
+{
+    public GameObject Player;
+    public UnityEngine.Video.VideoPlayer Video;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Video.Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Video.Play();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Video.Pause();
+        }
+    }
+}
