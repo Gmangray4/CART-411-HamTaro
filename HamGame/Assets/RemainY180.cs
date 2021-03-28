1fd741bd6887fbdd69c897f1fe1d121798ea2f77
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2021-03-28 18:07:31 -0400

    AI Cop in progress

---
+++ HamGame/Assets/RemainY180.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemainY180 : MonoBehaviour
+{
+
+    public GameObject plane;
+    // Start is called before the first frame update
+  
+    
+    Transform t;
+    public float fixedRotation = 180;
+
+    void Start()
+    {
+        t = transform;
+    }
+
+    void Update()
+    {
+        plane.transform.eulerAngles = new Vector3(t.eulerAngles.x, fixedRotation, t.eulerAngles.z);
+    }
+}
