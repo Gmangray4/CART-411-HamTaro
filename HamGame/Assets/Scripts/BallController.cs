4686dd96611aaeb16f64b4c2edd1bfb58f26bef8
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2021-04-03 20:17:36 -0400

    Ball remade

---
+++ HamGame/Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public float Speed;
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(xSpeed, 0.0f, ySpeed);
+
+        rb.AddForce (movement * Speed);
+
+
+    }
+}
---
+++ HamGame/Assets/Scripts/EnterBall.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterBall : MonoBehaviour
+{
+    private bool inVehicle = false;
+    BallController BallControls;
+    public GameObject guiObj;
+    GameObject player;
+
+    public Camera camplayer;
+    public Camera camCar;
+
+    void Start()
+    {
+        BallControls = GetComponent<BallController>();
+        player = GameObject.FindWithTag("Player");
+        guiObj.SetActive(false);
+        camplayer.enabled = true;
+        camCar.enabled = false;
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && inVehicle == false)
+        {
+            guiObj.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                guiObj.SetActive(false);
+                player.transform.parent = gameObject.transform;
+                BallControls.enabled = true;
+                player.SetActive(false);
+                inVehicle = true;
+                camCar.enabled = true;
+                camplayer.enabled = false;
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            guiObj.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        if (inVehicle == true && Input.GetKey(KeyCode.F))
+        {
+            BallControls.enabled = false;
+            player.SetActive(true);
+            player.transform.parent = null;
+            inVehicle = false;
+            camplayer.enabled = true;
+            camCar.enabled = false;
+
+
+        }
+    }
+}
