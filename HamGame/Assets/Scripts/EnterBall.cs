672190d3c52d46b649391957eab6e0b16d7e0779
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2021-04-12 21:23:41 -0400

    Bug Fixes for the Ball/EnteringExitingBallS Set to E/Col fixes

--- HamGame/Assets/Scripts/EnterBall.cs
+++ HamGame/Assets/Scripts/EnterBall.cs
@@ -8,10 +8,13 @@ public class EnterBall : MonoBehaviour
     BallController BallControls;
     public GameObject guiObj;
     GameObject player;
+    public GameObject playerSprite;
     public GameObject Ball;
 
     public Camera camplayer;
     public Camera camCar;
+
+    bool enterCoolDown;
 
     void Start()
     {
@@ -28,15 +31,18 @@ public class EnterBall : MonoBehaviour
         if (other.gameObject.tag == "Player" && inVehicle == false)
         {
             guiObj.SetActive(true);
-            if (Input.GetKey(KeyCode.E))
+            if (Input.GetKey(KeyCode.E) && enterCoolDown == false)
             {
                 guiObj.SetActive(false);
                 player.transform.parent = gameObject.transform;
                 BallControls.enabled = true;
                 player.SetActive(false);
+                playerSprite.SetActive(false);
                 inVehicle = true;
                 camCar.enabled = true;
                 camplayer.enabled = false;
+                enterCoolDown = true;
+                StartCoroutine(timer());
             }
         }
     }
@@ -49,16 +55,27 @@ public class EnterBall : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if (inVehicle == true && Input.GetKey(KeyCode.F))
+        if (inVehicle == true && Input.GetKey(KeyCode.E) && enterCoolDown == false)
         {
             BallControls.enabled = false;
             player.SetActive(true);
+            playerSprite.SetActive(true);
             player.transform.parent = null;
             inVehicle = false;
             camplayer.enabled = true;
             camCar.enabled = false;  
             player.transform.position = new Vector3(Ball.transform.position.x, Ball.transform.position.y + 5, Ball.transform.position.z);
-
+            playerSprite.transform.position = player.transform.position;
+            enterCoolDown = true;
+            StartCoroutine(timer());
         }
+    }
+
+    IEnumerator timer()
+    {
+
+        Debug.Log("Your enter Coroutine at" + Time.time);
+        yield return new WaitForSeconds(1.0f);
+        enterCoolDown = false;
     }
 }
