93f4d2edbe821886d1c5460bd0e83336bba29ef9
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2021-04-13 19:44:03 -0400

    Controls for Map  and UI added

---
+++ HamGame/Assets/Scripts/OpenMap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMap : MonoBehaviour
+{
+    public GameObject Map;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Map.gameObject.SetActive(!Map.gameObject.activeSelf);
+        }
+
+        if (Map.gameObject.activeSelf == true)
+        {
+
+        }
+        if (Map.gameObject.activeSelf == false)
+        {
+
+        }
+
+    }
+}
