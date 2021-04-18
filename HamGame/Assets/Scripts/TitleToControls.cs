18e33e8c3c01efd7c770f2ffe5c06f75c5cf6481
Gordon Gray <31903322+Gmangray4@users.noreply.github.com>
2021-04-17 22:05:36 -0400

    Last fixes of the night xD

--- HamGame/Assets/Scripts/TitleToControls.cs
+++ HamGame/Assets/Scripts/TitleToControls.cs
@@ -5,29 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class TitleToControls : MonoBehaviour
 {
-    public GameObject controlsImg;
-    public int count;
-
+    //public GameObject controlsImg;
+   // public int count;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        controlsImg.SetActive(false);
+        ///controlsImg.SetActive(false);
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If press ENTER, make the Controls image show up
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            controlsImg.SetActive(true);
-            count++;
-        }
+  
+   
 
         // If press SPACE, when the Controls image is up, the scene goes to the Intro scene
-        if (controlsImg == true && count == 1 &&  Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(sceneName: "Scenes/Intro");
+            SceneManager.LoadScene(sceneName: "Scenes/Controls");
         }
+
+       
     }
+
 }
